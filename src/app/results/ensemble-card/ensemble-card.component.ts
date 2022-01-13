5679d70443d7780f8e3f3b93bf25f285f74e5ebc
András Hatos <hatosandras@users.noreply.github.com>
2022-01-13 18:28:51 +0100

    add ensemble view

--- src/app/results/ensemble-card/ensemble-card.component.ts
+++ src/app/results/ensemble-card/ensemble-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {environment} from '../../../environments/environment';
 import {BehaviorSubject} from 'rxjs';
+import {ResultsService} from '../../services/results.service';
 
 @Component({
   selector: 'app-ensemble-card',
@@ -9,12 +10,33 @@ import {BehaviorSubject} from 'rxjs';
 })
 export class EnsembleCardComponent implements OnInit {
   @Input() currEnsmeble: object;
-  @Input() currConstructs: object[];
   currChainName = new BehaviorSubject<string>('');
-  submissionServerURL = environment.submission_server;
-  constructor() { }
+  currStatData = {
+    relative_asa_mean: 0.0,
+    entropy_dssp_mean: 0.0,
+    rg_mean: 0.0
+  };
+  currLinks = {
+    ramachandran_plot: '',
+    rg_boxplot: ''
+  };
+
+  constructor(public resultsService: ResultsService) { }
 
   ngOnInit(): void {
+    this.currChainName.next(this.resultsService.entryObj.constructs.sort((a, b) => a.chain_name > b.chain_name ? 1 : -1)[0]['chain_name']);
+    this.currChainName.subscribe(currChainName => {
+      this.currStatData = this.resultsService.entryObj['data_per_chain'].filter(x => x.ensemble_id === this.currEnsmeble['ensemble_id'] && x.chain === currChainName)[0] || this.currStatData;
+      if (this.resultsService.currViewMode === 'scheduler') {
+        this.currLinks.ramachandran_plot =  environment.submission_server + '/task/' + this.resultsService.currentUUID + '/file/submission/figure/' + this.currEnsmeble['ensemble_id'] + '_' +  this.currChainName.value + '_rama_angles.svg';
+        this.currLinks.rg_boxplot =  environment.submission_server + '/task/' + this.resultsService.currentUUID + '/file/submission/figure/' + this.currEnsmeble['ensemble_id'] + '_' +  this.currChainName.value + '_rg_boxplot_NG.svg';
+      } else {
+        this.currLinks.ramachandran_plot =  environment.ws + 'ramachandran_plot/' + this.currEnsmeble['ensemble_id'] + '/' +  this.currChainName.value;
+        this.currLinks.rg_boxplot =  environment.ws + 'rg_boxplot/' + this.currEnsmeble['ensemble_id'] + '/' + this.currChainName.value;
+      }
+
+
+    });
   }
 
 }
--- src/app/results/result-page/result-page.component.ts
+++ src/app/results/result-page/result-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InternalService } from '../../services/internal.service';
 import {ActivatedRoute} from '@angular/router';
+import {ResultsService} from '../../services/results.service';
 
 @Component({
   selector: 'app-result-page',
@@ -11,13 +12,16 @@ export class ResultPageComponent implements OnInit {
   entryObj = {};
 
   constructor(private internalService: InternalService,
+              private resultsService: ResultsService,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    const currentUUID = this.route.snapshot.paramMap.get('identifier');
+    this.resultsService.currentUUID = this.route.snapshot.paramMap.get('identifier');
  // console.log(currentUUID);
-    this.internalService.getFile(currentUUID, 'output_metadata.json').subscribe(currObj => {
+    this.internalService.getFile(this.resultsService.currentUUID, 'submission/metadata.json').subscribe(currObj => {
+      this.resultsService.parse(currObj);
       this.entryObj = currObj;
+      this.resultsService.currViewMode = 'scheduler';
       // console.log(this.entryObj);
     });
 

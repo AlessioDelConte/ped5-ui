933f4d70391a514283d0cb31eaf3eb2f07915841
Ivan Mičetić <ivanmicetic@users.noreply.github.com>
2022-03-14 12:24:31 +0100

    Update environments and budgets in angular.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   ws: window.location.protocol + '//' + window.location.hostname + '/api/',
-  submission_server: 'https://scheduler.biocomputingup.it/'
+  submission_server: 'https://scheduler.biocomputingup.it/',
+  pmcURL: 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?resultType=core&cursorMark=*&pageSize=25&format=json&query=',
+  enaTaxonomy: 'https://www.ebi.ac.uk/ena/taxonomy/rest/tax-id/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: false,
-  // ws: 'https://fuzpred.org/api/'
   ws: 'https://127.0.0.1:8000/',
   submission_server: 'https://scheduler.biocomputingup.it/',
   pmcURL: 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?resultType=core&cursorMark=*&pageSize=25&format=json&query=',
   enaTaxonomy: 'https://www.ebi.ac.uk/ena/taxonomy/rest/tax-id/'
 };
+
+console.log('Running default environment');
+console.log('environment.ws: ' + environment.ws);

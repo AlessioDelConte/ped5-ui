ae4dc416e5779e4e505f2f21db2f359c8dddf676
Luiggi Tenorio Ku <luiggi.tenorio@pucp.pe>
2023-05-16 15:24:39 +0200

    add dev scripts

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fuzpred-ui'),
+      dir: require('path').join(__dirname, './coverage/ped5-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  ws: 'https://dev.deposition.proteinensemble.org/api/v1/',
+  submission_server: 'https://scheduler.biocomputingup.it/'
+};
